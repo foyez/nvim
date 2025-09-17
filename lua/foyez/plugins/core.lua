5d081f2872103def6c6b0c45143880a5e931cab4
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-09-17 21:29:24 +0200

    add inline plugin for better filename per window

--- lua/foyez/plugins/core.lua
+++ lua/foyez/plugins/core.lua
@@ -130,23 +130,51 @@ return {
 						},
 					},
 				},
-				winbar = {
-					lualine_c = {
-						{
-							"filename",
-							path = 1, -- relative path for each active window
-						},
-					},
-				},
-				inactive_winbar = {
-					lualine_c = {
-						{
-							"filename",
-							path = 1, -- relative path for each inactive window
-						},
-					},
-				}
 			})
+    end,
+  },
+
+  -- 🪟 Incline: Per-window floating winbar
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",        -- lazy load on buffer read
+    priority = 1200,             -- load early if needed
+    dependencies = {
+      "craftzdog/solarized-osaka.nvim",
+      "nvim-tree/nvim-web-devicons", -- icons for files
+    },
+    config = function()
+      -- Load color palette
+      local colors = require("solarized-osaka.colors").setup()
+
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guifg = colors.base04, guibg = colors.magenta500 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = {
+          margin = { vertical = 0, horizontal = 1 }, -- remove extra vertical space
+        },
+        hide = {
+          cursorline = true, -- hide when cursorline is active
+        },
+        render = function(props)
+          -- get relative filename (tail)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+
+          -- prepend [+] if buffer is modified
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          -- get icon + color
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
     end,
   },
 
